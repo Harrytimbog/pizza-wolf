1134da4b74aa9e0c47a01690421a67d2e4391c11
Timilehin Arigbede <Harriemannie@gmail.com>
2022-02-16 17:08:54 +0100

    Integrate Paypal payment and cash for order

---
+++ pages/api/orders/[id].js
@@ -0,0 +1,24 @@
+import dbConnect from '../../../utils/mongo';
+import Order from '../../../models/Order';
+
+const handler = async (req, res) => {
+  const {
+    method,
+    query: { id },
+  } = req;
+
+  await dbConnect();
+
+  if (method === 'GET') {
+    try {
+      const order = Order.findById(id);
+      res.status(200).json(order);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+  if (method === 'PUT') {
+  }
+  if (method === 'DELETE') {
+  }
+};
