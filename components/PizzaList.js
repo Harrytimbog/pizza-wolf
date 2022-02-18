e1668a890d4e6ce12ae1a780c4ed7cab16365c6a
Timilehin Arigbede <Harriemannie@gmail.com>
2022-02-18 21:13:16 +0100

    Change restaurant tagline

--- components/PizzaList.js
+++ components/PizzaList.js
@@ -6,11 +6,9 @@ const PizzaList = ({ pizzaList }) => {
     <div className={styles.container}>
       <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
       <p className={styles.desc}>
-        Good time, Great taste Expect the best Eat healthy, Live more Food,
-        Folks and Fun | Good time, Great taste Expect the best Eat healthy, Live
-        more Food, Folks and Fun | Deliciousness jumping into the mouth You
-        deserve a break today As nature teaches us We do chicken right Chase the
-        flavors Finger Lickin | Oasis of pleasure
+        Great taste Expect the best | Eat healthy | Deliciousness jumping into
+        the mouth | You deserve a break today | Chase the flavors Finger Lickin
+        | Oasis of pleasure
       </p>
       <div className={styles.wrapper}>
         {pizzaList.map((pizza) => (
