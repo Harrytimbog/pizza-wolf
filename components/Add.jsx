7a5745e4317045b3085ff51e6b8d1b62da23a0a9
Timilehin Arigbede <Harriemannie@gmail.com>
2022-02-18 16:58:46 +0100

    Serve API end points from env file

--- components/Add.jsx
+++ components/Add.jsx
@@ -44,7 +44,7 @@ const Add = ({ setClose }) => {
         image: url,
       };
 
-      await axios.post('http://localhost:3000/api/products', newProduct);
+      await axios.post(`${process.env.BASE_URL}/api/products`, newProduct);
       setClose(true);
     } catch (err) {
       console.log(err);
